44243d4058eb5361582c16386c0291b4b6e90ddd
RoverLiu <liudechuan99@gmail.com>
2023-05-04 23:57:21 +1000

    update for box object (fix size, data log)

--- environments/manipulation/push_away.py
+++ environments/manipulation/push_away.py
@@ -451,7 +451,7 @@ class PushAway(SingleArmEnv):
         self.cube = BoxObject(
             name="cube",
             size_min=[0.12, 0.12, 0.12],  # [0.015, 0.015, 0.015],
-            size_max=[0.15, 0.15, 0.15],  # [0.018, 0.018, 0.018])
+            size_max=[0.12, 0.12, 0.12],  # [0.018, 0.018, 0.018])
             rgba=[1, 0, 0, 1],
             density=30,
             material=redwood,
